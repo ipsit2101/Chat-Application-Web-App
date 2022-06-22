699ca832e03999995b6a563bdd374c89714c332b
Ipsit <ishukr2101@gmail.com>
2022-06-23 02:37:31 +0530

    scaffolding the project

---
+++ src/Misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCeq81rApxH9VFkyHWD40TsPGMAqmrz-tQ",
+    authDomain: "chat-application-957c9.firebaseapp.com",
+    databaseURL: "https://chat-application-957c9-default-rtdb.firebaseio.com",
+    projectId: "chat-application-957c9",
+    storageBucket: "chat-application-957c9.appspot.com",
+    messagingSenderId: "644974979814",
+    appId: "1:644974979814:web:78c488c1f93dde9f66e927"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
