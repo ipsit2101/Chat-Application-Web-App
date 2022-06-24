1b2fe0ea2bc31d0522dfecb67902ed38632e3563
Ipsit <ishukr2101@gmail.com>
2022-06-24 17:07:18 +0530

    Sign Out functionality

--- src/My Components/Dashboard/Dashboard.js
+++ src/My Components/Dashboard/Dashboard.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { Alert, Button, Drawer } from "rsuite";
+import { useProfile } from "../../Context/profileContext";
+import { useOpen } from "../../Misc/CustomHooks";
+import { auth } from "../../Misc/firebase";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+
+  const { profile } = useProfile();
+  const { close } = useOpen();
+
+  const onSignOut = useCallback(() => {
+    auth.signOut();
+    Alert.info('Signed Out', 4000);
+
+    close();
+  }, [close]);
+    
+  return (
+    <>
+      <Drawer.Header>
+        <Drawer.Title>
+          Dashboard
+        </Drawer.Title>
+      </Drawer.Header>
+
+      <Drawer.Body>
+        <h3>Hey {profile.name}</h3>
+      </Drawer.Body>
+     
+      <Drawer.Footer>
+        <Button block color="red" onClick={onSignOut}>Sign Out</Button>
+      </Drawer.Footer>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/My Components/Dashboard/DashboardToggle.js
+++ src/My Components/Dashboard/DashboardToggle.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Button, Drawer, Icon } from 'rsuite';
-import { useOpen } from '../../Misc/CustomHooks';
+import { useMediaQuery, useOpen } from '../../Misc/CustomHooks';
 import Dashboard from './Dashboard';
 
 const DashboardToggle = () => {
   const { isOpen, open, close } = useOpen();
+  const isMobile = useMediaQuery('(max-width: 992px)');
 
   return (
     <>
       <Button block color='green' onClick = {open}>
         <Icon icon="dashboard" /> Dashboard
       </Button>
-      <Drawer show = {isOpen} onHide = {close} placement = 'left'>
+      <Drawer full = {isMobile} show = {isOpen} onHide = {close} placement = 'left'>
         <Dashboard />
       </Drawer>
     </>
