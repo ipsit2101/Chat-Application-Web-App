556463e7a00160425b3a6ffad98e752478b9296e
Ipsit <ishukr2101@gmail.com>
2022-06-28 17:57:49 +0530

    displaying messages

--- src/My Components/Chat-Window/Message-Window/index.js
+++ src/My Components/Chat-Window/Message-Window/index.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router';
+import { database } from '../../../Misc/firebase';
+import { TransformToArray } from '../../../Misc/Helpers';
+import MessageItem from './MessageItem';
 
 const MessageWindow = () => {
+
+  const { chatID } = useParams();
+  const [message , setMessage] = useState(null);
+
+  const isChatEmpty = message && message.length === 0;
+  const canShowMessage = message && message.length > 0;
+
+  useEffect(() => {
+
+    const messageRef = database.ref('/messages');
+    messageRef.orderByChild('roomId').equalTo(chatID).on('value', (snapshot) => {
+      const data = TransformToArray(snapshot.val());
+      setMessage(data);
+    })
+
+    return () => {
+      messageRef.off('value');
+    }
+
+  }, [chatID]);
+
   return (
-    <div>
-      messages
-    </div>
+    <ul className='msg-list custom-scroll'>
+      { isChatEmpty && <li>No messages yet</li> }
+      {canShowMessage && message.map(msg => <MessageItem key = {msg.id} message = {msg} />)}
+    </ul>
   )
 }
 
